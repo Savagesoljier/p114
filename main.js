1d80afbdef6c5247515129fc89d537cb012349d3
Savagesoljier <78633767+Savagesoljier@users.noreply.github.com>
2021-08-25 18:31:31 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function preload() {}
+
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.position(500,150);
+}
+function draw(){}
+function takeSnapshot(){
+    save("myfilterimage");
+}
